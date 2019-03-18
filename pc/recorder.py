a08c0dc942d4f72557dd1e416a3d486caf713a63
Matthias Larisch <mail@matthias-larisch.de>
2019-03-18 22:36:48 +0100

    Change settings from PC application

--- pc/recorder.py
+++ pc/recorder.py
@@ -1,7 +1,7 @@
 import sys
 import argparse
 from intelhex import IntelHex
-from bluepy.btle import Scanner, DefaultDelegate, Peripheral, ADDR_TYPE_PUBLIC
+from bluepy.btle import Scanner, DefaultDelegate, Peripheral, ADDR_TYPE_RANDOM
 from struct import pack, unpack
 from enum import Enum
 
@@ -16,11 +16,13 @@ class NotificationDelegate(DefaultDelegate):
 
 class Recorder():
     RECORDER_SERVICE_UUID = "8965c8d0-b543-9695-6414-789670ac1cf7"
-    RECORDER_CONTROL_UUID = "8e921d16-a929-4c0c-41ed-8e40fa37013f"
-    RECORDER_DATA_UUID = "8c817150-dfc8-f45d-7d8e-e8a539c4a101"
+    RECORDER_CONTROL_UUID ="8e921d16-a929-4c0c-41ed-8e40fa37013f"
+    RECORDER_DATA_UUID =   "8c817150-dfc8-f45d-7d8e-e8a539c4a101"
+    RECORDER_STATUS_UUID = "dfc54332-8dda-7f88-2699-630483c2fd99"
     peripheral = None
     service = None
     control_char = None
+    status_char = None
     data_char = None
     readout_in_progress = False
     remaining_packets = 0
@@ -34,19 +36,40 @@ class Recorder():
                 self.control_char = c
             elif c.uuid == self.RECORDER_DATA_UUID:
                 self.data_char = c
+            elif c.uuid == self.RECORDER_STATUS_UUID:
+                self.status_char = c
         if (self.service is None or
             self.control_char is None or
-            self.data_char is None):
+            self.data_char is None or
+            self.status_char is None):
                 raise Exception("Could not find all characteristics in recorder service on %s" % (peripheral))
 
         print("Initialized recorder application")
         self.delegate = NotificationDelegate(self)
         self.peripheral.withDelegate(self.delegate)
 
+    def get_status(self):
+        status = self.status_char.read()
+        flags, recorder_size = unpack("<IH10x", status)
+        print("Flags: %8X recorder buffer used: %d" % (flags, recorder_size))
+        return flags, recorder_size
+
     def write_readout_command(self, size):
         data = pack("<BH", 1, size)
         self.control_char.write(data, True)
         print("Requested to read %d data points" % (size))
+
+    def set_measurement_interval(self, interval):
+        data = pack("<BH", 4, interval)
+        self.control_char.write(data, True)
+
+    def reset_storage(self):
+        data = pack("<B", 5)
+        self.control_char.write(data, True)
+
+    def reset_to_ota(self):
+        data = pack("<B", 6)
+        self.control_char.write(data, True)
 
     def register_indication(self, valHandle, enabled):
         data = pack("<H", 2 if enabled else 0)
@@ -64,8 +87,21 @@ class Recorder():
 
     def notification(self, handle, data):
         if handle == self.data_char.getHandle():
-            unpacked = unpack("<10h", data)
-            print("Received data: %d %d %d %d %d %d %d %d %d %d" % (unpacked))
+            unpacked = unpack("<10H", data)
+            for d in unpacked:
+                if d == 0x8003:
+                    print("Start measurement")
+                elif d == 0x8002:
+                    print("Stop measurement")
+                elif d == 0xBFFF:
+                    print("()")
+                elif d & 0xC000:
+                    print("Measurement interval: %d s" % (d & 0x3FFF))
+                else:
+                    if d & 0x4000:
+                        d = -d
+                    print("%f °C" % (d / 100))
+                
             self.remaining_packets = self.remaining_packets - 10
             if self.remaining_packets <= 0:
                 self.readout_in_progress = False
@@ -79,6 +115,9 @@ def main():
     parser.add_argument("--scan", help="Scans for Bluetooth low energy devices", action="store_true")
     parser.add_argument("--device", help="Connects to given bluetooth device")
     parser.add_argument("--read", help="Read given number of measurements from given device", type=int)
+    parser.add_argument("--set_measurement_interval", help="take a measurement every X seconds", type=int)
+    parser.add_argument("--reset_storage", help="Empty the measurement buffer", action="store_true")
+    parser.add_argument("--reset_to_bootloader", help="Reset thermometer (losing all data) to bootloader for firmware update", action="store_true")
     args = parser.parse_args()
 
     if args.scan:
@@ -91,11 +130,24 @@ def main():
                 print("  %s = %s" % (desc, value))
 
     if args.device:
-        peripheral = Peripheral(args.device, ADDR_TYPE_PUBLIC)
+        peripheral = Peripheral(args.device, ADDR_TYPE_RANDOM)
         recorder = Recorder(peripheral)
+        recorder.get_status()
 
     if args.device and args.read:
         recorder.read(args.read)
+
+    if args.device and args.reset_storage:
+        recorder.reset_storage()
+        print("Reset device storage")
+
+    if args.device and args.reset_to_bootloader:
+        recorder.reset_to_ota()
+        print("Device should be in OTA mode now")
+
+    if args.device and args.set_measurement_interval:
+        recorder.set_measurement_interval(args.set_measurement_interval)
+        print("Measurement interval: %d s" % (args.set_measurement_interval))
 
 
 if __name__ == "__main__":
